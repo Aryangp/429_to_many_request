8c85ddc792ed3c8effa7681bbf0c412543f0f564
Aryangp <guptaaryan0510@gmail.com>
2024-05-25 20:31:10 +0530

    test and the ser er is working correctly

--- backend2/middleware/weaviate.py
+++ backend2/middleware/weaviate.py
@@ -8,29 +8,32 @@ import io
 load_dotenv()
 
 def weaviate_init():
-    client = weaviate.Client(
-        url = "https://weaviatetest-8o6sm485.weaviate.network",  # Replace with your endpoint
-        auth_client_secret=weaviate.AuthApiKey(api_key=os.getenv("WEAVIATE_API_KEY")),  # Replace w/ your Weaviate instance API key
-        additional_headers = {
-            "X-HuggingFace-Api-Key": os.getenv("HUGGINGFACE_API_KEY")  # Replace with your Hugging Face API key
-        }
-    )
     # client = weaviate.Client(
-    #     #http://34.168.110.14:8080
-    #     url = "http://35.230.98.126:8080",  # Replace with your endpoint
+    #     url = "https://weaviatetest-8o6sm485.weaviate.network",  # Replace with your endpoint
+    #     auth_client_secret=weaviate.AuthApiKey(api_key=os.getenv("WEAVIATE_API_KEY")),  # Replace w/ your Weaviate instance API key
+    #     additional_headers = {
+    #         "X-HuggingFace-Api-Key": os.getenv("HUGGINGFACE_API_KEY")  # Replace with your Hugging Face API key
+    #     }
     # )
+    client = weaviate.Client(
+        #http://34.168.110.14:8080
+        url = "http://34.168.110.14:8080",  # Replace with your endpoint
+    )
     return client
 
-def weaviate_middleware_search(request,db,current_data,query):
+# new_df=df[['title','brand','description','price','specifications']]
+
+def weaviate_middleware_search(current_data,query):
     className = current_data[1].className
     client = weaviate_init()
+    # TOVJeAggmG
     response = (
         client.query
-        .get(className, ["unique_id", "product_name","category","sub_category","brand","sale_price","market_price","product_type","rating","product_desc"])
-        .with_near_text({
-            "concepts": [query],
-        })
-        .with_limit(4)
+        .get(className, ["title","description","raw_description","price","specifications","primary_category","main_image"])
+        .with_hybrid(
+            query=query,
+        )
+        .with_limit(20)
         .with_additional(["distance"])
         .do()
     )
@@ -47,27 +50,27 @@ def weaviate_filter_price(data,className,query):
     print(lower_price_limit)
     response = (
         client.query
-        .get(className, ["unique_id", "product_name","category","sub_category","brand","sale_price","market_price","product_type","rating","product_desc"])
-        .with_near_text({
-            "concepts": [query],
-        })
+        .get(className, ["title","description","raw_description","price","specifications","primary_category","main_image"])
+        .with_hybrid(
+            query=query,
+        )
         .with_where({
             "operator": "And",
             "operands": [
                 {
-                    "path": ["sale_price"],
+                    "path": ["price"],
                     "operator": "GreaterThan",
                     "valueInt": lower_price_limit
                 },
                 {
-                    "path": ["sale_price"],
+                    "path": ["price"],
                     "operator": "LessThan",
                     "valueInt": upper_price_limit
                 }
             ]
         })
         .with_limit(10)
-        .with_additional(["distance"])
+        .with_additional(["score"])
         .do()
     )
 
@@ -79,26 +82,21 @@ def weaviate_create_schema(className,properties):
     class_search_schema={
         "class": className,
         "description": f"A class called {className} and used for doing vector search on {className} data.",
-        "vectorizer": "text2vec-huggingface",
+        "vectorizer": "text2vec-transformers",
         "vectorIndexType": "hnsw",# can be set flat for that refer https://weaviate.io/developers/weaviate/manage-data/collections
 
         "vectorIndexConfig": {
-        "pq": {
-        "enabled": True,
-        "trainingLimit": 100000,
-        "segments": 512 # refer here https://weaviate.io/developers/weaviate/concepts/vector-index#:~:text=HNSW%20is%20an%20algorithm%20that,(adding%20data%20with%20vectors).
-        },
-        "distance": "cosine",
+            "pq": {
+                "enabled": True,
+                "trainingLimit": 100000,
+                "segments": 512 # refer here https://weaviate.io/developers/weaviate/concepts/vector-index#:~:text=HNSW%20is%20an%20algorithm%20that,(adding%20data%20with%20vectors).
+            },
+            "distance": "cosine",
         },
         "moduleConfig": {
-            "text2vec-huggingface": {
-            "model": "sentence-transformers/all-MiniLM-L6-v2",
-            "options": {
-                "waitForModel": True,
-                "useGPU": False,
-                "useCache": True
-            },
-            "vectorizeClassName": False
+            "text2vec-transformers": {
+                "vectorizeClassName": False,
+                "inferenceUrl":"http://t2v-transformers:8080"
             }
         },
         "properties": properties
@@ -114,123 +112,23 @@ def weaviate_create_schema(className,properties):
     #         "dataType": ["int"],
     #         "description": "user id",
     #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": True,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-    #         {
-    #         "name": "product_name",
-    #         "dataType": ["text"],
-    #         "description": "product_name",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": True,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-
-    #         {
-    #         "name": "product_desc",
-    #         "dataType": ["text"],
-    #         "description": "product description",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": True,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-
-    #         {
-    #         "name": "category",
-    #         "dataType": ["text"],
-    #         "description": "category of product",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
+    #             "text2vec-transformers": {
     #             "skip": False,
     #             "vectorizePropertyName": True
     #             }
     #         }
     #         },
-    #         {
-    #         "name": "sub_category",
-    #         "dataType": ["text"],
-    #         "description": "sub category of product",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": False,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-    #         {
-    #         "name": "brand",
-    #         "dataType": ["text"],
-    #         "description": "product brand",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": False,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-    #         {
-    #         "name": "sale_price",
-    #         "dataType": ["number"],
-    #         "description": "color of product",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": False,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-    #         {
-    #         "name": "product_type",
-    #         "dataType": ["text"],
-    #         "description": "type of product",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": False,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-    #         {
-    #         "name": "market_price",
-    #         "dataType": ["number"],
-    #         "description": "product title",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": False,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         },
-    #         {
-    #         "name": "rating",
-    #         "dataType": ["number"],
-    #         "description": "rating",
-    #         "moduleConfig": {
-    #             "text2vec-huggingface": {
-    #             "skip": False,
-    #             "vectorizePropertyName": True
-    #             }
-    #         }
-    #         }
     #     ]
 
 
 # helper function to add data to weaviate
-def add_data(data1):
+def add_data(data1,className):
     client=weaviate_init()
     uuid = client.data_object.create(
-         class_name="CatalogSearchWithDescription",
+         class_name= className,
          data_object=data1
     )
+    print(uuid)
     return uuid
 
 def get_objects(uuid,className):
@@ -251,6 +149,7 @@ def convert_data(df_new):
 
 # Main function to add the data to weaviate
 def weaviate_middleware_add_data(request,current_user,db,fileObject):
+    className=current_user[1].className
     if fileObject.filename == '':
         raise HTTPException(status_code=400, detail="No file selected for uploading")
     
@@ -265,7 +164,7 @@ def weaviate_middleware_add_data(request,current_user,db,fileObject):
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
         print("no error till now")
-        percentage = 10
+        percentage = 100
             # Check if there are any null values in the DataFrame
         if df.isnull().values.any():
             # Iterate over each column
@@ -279,11 +178,10 @@ def weaviate_middleware_add_data(request,current_user,db,fileObject):
         # Use the sample method to randomly select the specified percentage of data
         test_data = df.sample(n=sample_size)
         test_data = test_data.round(4)  # Adjust the precision as needed
-        test_data_new=test_data.head(200)
-        dict_df=convert_data(test_data_new)
+        dict_df=convert_data(test_data)
         try:
             for data in dict_df:
-                uuid=add_data(data)
+                uuid=add_data(data,className)
             return json.dumps({"message":"Data added successfully","uuid":uuid},indent=2)
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
